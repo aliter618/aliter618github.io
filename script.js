598298cae03ea640a555ed57ef4c396afb00f995
aliter618 <1709367009@qq.com>
2026-08-18 18:49:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ yesButton.addEventListener("click", function () {
   document.body.innerHTML = `
         <div class="yes-screen">
             <h1 class="yes-text"></h1>
-            <img src="images/hug.png" alt="拥抱" class="yes-image">
+            <img src="iimages/13431523676244858.png" alt="拥抱" class="yes-image">
         </div>
     `;
 
